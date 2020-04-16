58df4b967a1553906bd8d23075f8129e37b4cab5
Paolo.loeffler <paolo.loeffler@daimler-x.com>
2020-04-17 01:20:44 +0200

    finished lecture no. 5

---
+++ real-world-vue/src/views/EventList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Events Listening</h1>
+
+    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
+      >Show Event {{ id }}</router-link
+    >
+  </div>
+</template>
